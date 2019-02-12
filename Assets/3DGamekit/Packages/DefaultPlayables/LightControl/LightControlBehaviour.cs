9de4fe0919602c4877edb237ae14c7aed92b018f
Remy <remym@unity3d.com>
2019-02-12 17:00:20 +0100

    First ulgy commit.

---
+++ Assets/3DGamekit/Packages/DefaultPlayables/LightControl/LightControlBehaviour.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[Serializable]
+public class LightControlBehaviour : PlayableBehaviour
+{
+    public Color color = Color.white;
+    public float intensity = 1f;
+    public float bounceIntensity = 1f;
+    public float range = 10f;
+}
